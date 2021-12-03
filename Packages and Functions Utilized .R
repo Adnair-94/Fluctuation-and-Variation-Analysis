56b2428c2747e2276edc21d8f9b6ff2872bd3684
AD Nair <56961458+Adnair-94@users.noreply.github.com>
2021-12-03 13:47:12 +0900

    Update Packages and Functions Utilized .R

--- Packages and Functions Utilized .R
+++ Packages and Functions Utilized .R
@@ -1,3 +1,4 @@
+#required libraries
 library(extrafont)
 loadfonts(device="win")
 windowsFonts()
@@ -5,7 +6,7 @@ library(isofor)
 library(tidyverse)
 library(ggpubr)
 library(ggsci)
-################
+#theme for journal publicaton (further modification can be done according to required specific graphs)
 journal_theme=theme(text=element_text(family="Arial",size=10),
 rect=element_blank(),
 panel.grid=element_blank(),
@@ -14,12 +15,13 @@ axis.ticks = element_line(colour = "black", size = 0.5),
 axis.text.x = element_text(color="black",size = 10, margin = unit(c(t = 2.5, r = 0, b = 0, l = 0), "mm")),
 axis.text.y = element_text(color="black",size = 10, margin = unit(c(t = 0, r = 2.5, b = 0, l = 0), "mm")),
 panel.border = element_rect(colour = "black", fill=NA, size=0.5))
-############################
-isofor<-function(x){
-model=iForest(x[-c(1)],nt=100)
-iso.score=predict(model,x[-c(1)])
-x$process_score=iso.score
-x$category=ifelse(x$process_score>=0.60,"Outlier","Normal")
+theme_set(journal_theme)
+#isolation forest analysis function made using the "isofor" package. Before using the function confirm the columns of the .csv file. This function includes all columns in the analysis except for the first.
+isofor<-function(x){ #where x is a data frame created with the .csv file.
+model=iForest(x[-c(1)],nt=100) #excludes first column from the analysis, uses 100 isolation trees, subsample (phi) not speficied.
+iso.score=predict(model,x[-c(1)]) #generates the isolation score for the data  
+x$process_score=iso.score #adds a column called process_score (using the generated isolation score) to the data frame.
+x$category=ifelse(x$process_score>=0.60,"Outlier","Normal") # adds a column called category to the data frame which labels the data as outlier or normal based on isolation score
 return(x)
 }
 ############
